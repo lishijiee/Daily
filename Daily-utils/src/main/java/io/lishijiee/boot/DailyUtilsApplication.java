0f95d2537c684a0cb9aa07e49a512c2756092310
lishijiee <2424782003@qq.com>
2023-03-05 23:05:10 +0800

    格式化pom配置

---
+++ Daily-utils/src/main/java/io/lishijiee/boot/DailyUtilsApplication.java
@@ -0,0 +1,13 @@
+package io.lishijiee.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyUtilsApplication.class, args);
+    }
+
+}
